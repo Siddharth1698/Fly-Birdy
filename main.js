fa793f51c823d1079d6651e2dbd00ba4176a4afd
Siddharth M <siddharth.m98@gmail.com>
2017-10-04 00:36:03 +0530

    Changing Bird and adding Sound Effects

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-var x;
+
 var mainState = {
 	preload: function(){
-		game.load.image('bird','assets/flappy1.gif');
+		game.load.image('bird','assets/redbird.gif');
 		game.load.image('pipe','assets/pipe1.png');
 		game.load.audio('jump', 'assets/jump.wav'); 
+		game.load.audio('bg', 'assets/bg1.m4a'); 
+		game.load.audio('dead', 'assets/dead1.m4a');
+
+
 
 	},
 	create: function()
 	{
-		game.stage.backgroundColor = '#71c5cf';
+		game.stage.backgroundColor = '#ffffff';
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		this.bird = game.add.sprite(100, 245, 'bird');
 		game.physics.arcade.enable(this.bird);
@@ -17,16 +21,21 @@ var mainState = {
 		spaceKey.onDown.add(this.jump,this);
 		this.pipes = game.add.group();
 		this.timer = game.time.events.loop(2000, this.addRowOfPipes, this);
+
         this.score = 0;
         this.labelScore = game.add.text(20, 20, "Score is: ", 
-          { font: "30px Arial", fill: "#ffffff" }); 
+          { font: "30px Arial", fill: "#191970" }); 
+        
        
         this.bird.anchor.setTo(-0.2, 0.5); 
         this.jumpSound = game.add.audio('jump'); 
+         this.bgSound = game.add.audio('bg'); 
+         this.bgSound.play();
+           this.deadSound = game.add.audio('dead'); 
 			},
 			
 	update: function()
-	{
+	{   
 		if (this.bird.y < 0 || this.bird.y > 490)
             	this.restartGame();
         if (this.bird.angle < 20)
@@ -46,12 +55,15 @@ var mainState = {
 	},
 	restartGame:function()
 	{
+		this.bgSound.resume();
+
+		
 		game.state.start('main');
 		
 
 	},
 	addOnePipe:function(x,y)
-	{
+	{   
 		var pipe = game.add.sprite(x,y,'pipe');
 		this.pipes.add(pipe);
 		game.physics.arcade.enable(pipe);
@@ -67,18 +79,23 @@ var mainState = {
 		   if(i != hole && i != hole+1)
 				{this.addOnePipe(400,i*60+10);}}
 			this.score += 1;
+			
 	
 
             this.labelScore.text =  "Score is: "+this.score;  
             
             
+            
 		
 	},
+
+	
 
 	hitpipe: function()
 	{
 		if(this.bird.alive == false)
 		{
+
 			return;
 		}
 		this.bird.alive = false;
@@ -86,11 +103,17 @@ var mainState = {
 
         this.pipes.forEach(function(p){
         	p.body.velocity.x = 0;
+        	this.bgSound.pause();
+        	
+        	this.deadSound.play(); 
+
 
         },this);
 
+
 	},
 };
+
 
 var game = new Phaser.Game(400, 490);
 game.state.add('main',mainState);
